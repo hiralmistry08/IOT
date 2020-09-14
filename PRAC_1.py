4a2e35dff95f075b6b9729677b86f0c557c6e1b8
hiralmistry08 <71007822+hiralmistry08@users.noreply.github.com>
2020-09-14 12:07:36 +0530

    Create PRAC_1.py

---
+++ PRAC_1.py
@@ -0,0 +1,17 @@
+import telepot
+token = '1353050608:AAF3fF8UoDz9rWlU40hcrRjQjsylFRQG33k'
+TelegramBot = telepot.Bot(token)
+print (TelegramBot.getMe())
+
+Hiral I'd:928689986
+
+
+https://api.telegram.org/bot1353050608:AAF3fF8UoDz9rWlU40hcrRjQjsylFRQG33k/getUpdates
+
+https://api.telegram.org/bot1353050608:AAF3fF8UoDz9rWlU40hcrRjQjsylFRQG33k/getme
+
+
+Urvashi chat I'd:850363090
+
+
+https://api.telegram.org/bot1353050608:AAF3fF8UoDz9rWlU40hcrRjQjsylFRQG33k/sendMessage?chat_id=850363090&text=Hello
